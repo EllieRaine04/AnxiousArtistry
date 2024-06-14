8490a936b944f5a8c08c02bc561743c07e6457ff
EllieRaine04 <elanande@ucsc.edu>
2024-06-14 11:47:20 -0700

    skeleton work

---
+++ js/site.js
@@ -0,0 +1,28 @@
+// index.js - Experimenting
+// Author: Elisa Anderson 
+// Date:5-14-24
+
+document.getElementById('art-gallery').addEventListener('click', function() {
+  alert('Navigating to the Art Gallery...');
+  // Code to navigate to the Art Gallery page
+});
+
+document.getElementById('interactive-journal').addEventListener('click', function() {
+  alert('Navigating to the Interactive Journal...');
+  // Code to navigate to the Interactive Journal page
+});
+
+document.getElementById('about-us').addEventListener('click', function() {
+  alert('Navigating to the About Us page...');
+  // Code to navigate to the About Us page
+});
+
+document.getElementById('resources').addEventListener('click', function() {
+  alert('Navigating to the Resources page...');
+  // Code to navigate to the Resources page
+});
+
+document.getElementById('our-team').addEventListener('click', function() {
+  alert('Navigating to the Our Team page...');
+  // Code to navigate to the Our Team page
+});
